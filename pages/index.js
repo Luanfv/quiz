2081e83ffe75983b39cf0a879a66ffa23b7217a9
Luan <luanfrancavieira@gmail.com>
2021-01-27 20:36:24 -0300

    third day

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,16 @@
 import React, { useState, useCallback } from 'react';
-import styled from 'styled-components';
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
+
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 function Home() {
   const router = useRouter();
@@ -35,32 +26,37 @@ function Home() {
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <QuizLogo />
+
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
+
+          <Widget.Content>
+            <form onSubmit={(event) => submit(event)}>
+              <Input
+                name="username"
+                placeholder="Digite seu nome"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
+
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
           <Widget.Content>
             <p>{db.description}</p>
           </Widget.Content>
         </Widget>
 
-        <Widget>
-          <Widget.Content>
-            <form onSubmit={(event) => submit(event)}>
-              <input
-                placeholder="Digite seu nome"
-                onChange={(event) => setName(event.target.value)}
-              />
-
-              <button type="submit" disabled={name.length === 0}>
-                {`Jogar ${name}`}
-              </button>
-            </form>
-          </Widget.Content>
-        </Widget>
-
         <Footer />
       </QuizContainer>
+
       <GitHubCorner projectUrl="https://github.com/Luanfv" />
     </QuizBackground>
   );
